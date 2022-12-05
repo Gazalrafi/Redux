72247ae6d3c2f489aa2d2c8551b61228d061308b
Gazalrafi <grafi7485>
2022-12-05 23:34:47 +0530

    redux fundamentals

---
+++ redux-demo.js
@@ -0,0 +1,44 @@
+const redux=require('redux');
+
+const counterReducer=(state={counter:1},action)=>{
+
+if(action.type === 'increment'){
+
+    return{
+        counter:state.counter+1,
+    };
+};
+
+if(action.type === 'decrement'){
+
+    return{
+        counter:state.counter-1,
+    };
+};
+
+return state;
+};
+
+const store=redux.createStore(counterReducer);
+
+console.log(store.getState()); //log after store is created
+
+const counterSubscriber=()=>{
+    const latestState=store.getState();
+    console.log(latestState);
+}
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({type:'increment'});
+store.dispatch({type:'increment'});
+
+store.dispatch({type:'increment'});
+
+store.dispatch({type:'increment'});
+
+store.dispatch({type:'increment'});
+
+
+store.dispatch({type:'decrement'});
+
